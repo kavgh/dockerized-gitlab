befa5af9bfb712957e18ad7ebaef7d70fb162c4c
kavgh <throttle@mailfence.com>
2025-02-21 12:50:59 +0200

    Disable sending usage data to gitlab.com

--- config/gitlab.rb
+++ config/gitlab.rb
@@ -1,3 +1,11 @@
 external_url "https://192.168.0.28"
 
 external_url "http://192.168.0.28"
+
+##=====================================================================##
+##======= Disable sending usage statistics sending to gitlab.com ======##
+##= https://docs.gitlab.com/administration/settings/usage_statistics/ =##
+##=====================================================================##
+
+gitlab_rails['include_optional_metrics_in_service_ping'] = false
+gitlab_rails['usage_ping_enabled'] = false
