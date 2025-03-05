d35a86306df6617bbf9b34a7e2c4d14fe0e9e0ad
kavgh <throttle@mailfence.com>
2025-03-05 12:28:20 +0200

    Renamed resources/issue-cert.sh file to resources/cleaning.sh also updated script so it now only clean temporary files

---
+++ resources/cleaning.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eo pipefail
+
+/usr/bin/docker events --filter 'event=start' --filter 'event=stop' --filter 'name=git-acme' --filter 'name=gitlab' |
+
+while read events; do
+  event=$(echo $events | grep -oE '\b(start|stop)\b')
+  name=$(echo $events | grep -oE 'name=(git-acme|gitlab)')
+  
+  if [[ "$event" == "stop" && "$name" == "name=git-acme" ]]; then
+    /usr/bin/sleep 5
+    /usr/bin/rm -rf /home/Unwoven/Projects/Docker/gitlab_server/step/certs /home/Unwoven/Projects/Docker/gitlab_server/step/db \
+    /home/Unwoven/Projects/Docker/gitlab_server/step/secrets/intermediate_ca_key /home/Unwoven/Projects/Docker/gitlab_server/step/secrets/root_ca_key \
+    /home/Unwoven/Projects/Docker/gitlab_server/step/templates /home/Unwoven/Projects/Docker/gitlab_server/step/config
+  elif [[ "$event" == "stop" && "$name" == "name=gitlab" ]]; then
+    /usr/bin/sleep 5
+    /usr/bin/rm -rf /home/Unwoven/Projects/Docker/gitlab_server/logs/* /home/Unwoven/Projects/Docker/gitlab_server/data/* /home/Unwoven/Projects/Docker/gitlab_server/data/.* \
+    /home/Unwoven/Projects/Docker/gitlab_server/config/ssl /home/Unwoven/Projects/Docker/gitlab_server/config/trusted-certs \
+    /home/Unwoven/Projects/Docker/gitlab_server/config/gitlab-secrets.json /home/Unwoven/Projects/Docker/gitlab_server/config/initial_root_password \
+    /home/Unwoven/Projects/Docker/gitlab_server/config/ssh_host_ecdsa_key* /home/Unwoven/Projects/Docker/gitlab_server/config/ssh_host_ed25519_key* \
+    /home/Unwoven/Projects/Docker/gitlab_server/config/ssh_host_rsa_key*
+  fi
+done
