9b03afd8240d4c4723509a074976dfb17eec83b7
kavgh <throttle@mailfence.com>
2025-04-04 15:19:45 +0300

    Updated .gitignore and cleaning.sh files so it more convinient test

--- resources/cleaning.sh
+++ resources/cleaning.sh
@@ -2,22 +2,16 @@
 
 set -eo pipefail
 
-/usr/bin/docker events --filter 'event=stop' --filter 'name=git-acme' --filter 'name=gitlab' |
+/usr/bin/docker events --filter 'event=destroy' --filter 'container=git-acme' |
 
 while read events; do
-  event=$(echo $events | grep -oE stop) #'\b(start|stop)\b')
-  name=$(echo $events | grep -oE 'name=(git-acme|gitlab)')
+  /usr/bin/rm -rf /home/Unwoven/Projects/Docker/gitlab_server/step/*
   
-  if [[ "$event" == "stop" && "$name" == "name=git-acme" ]]; then
-    /usr/bin/sleep 5
-    /usr/bin/rm -rf /home/Unwoven/Projects/Docker/gitlab_server/step/*
-  elif [[ "$event" == "stop" && "$name" == "name=gitlab" ]]; then
-    /usr/bin/sleep 5
-    /usr/bin/rm -rf /home/Unwoven/Projects/Docker/gitlab_server/logs/* /home/Unwoven/Projects/Docker/gitlab_server/data/* \
-    /home/Unwoven/Projects/Docker/gitlab_server/data/.* \
-    /home/Unwoven/Projects/Docker/gitlab_server/config/ssl /home/Unwoven/Projects/Docker/gitlab_server/config/trusted-certs \
-    /home/Unwoven/Projects/Docker/gitlab_server/config/gitlab-secrets.json /home/Unwoven/Projects/Docker/gitlab_server/config/initial_root_password \
-    /home/Unwoven/Projects/Docker/gitlab_server/config/ssh_host_ecdsa_key* /home/Unwoven/Projects/Docker/gitlab_server/config/ssh_host_ed25519_key* \
-    /home/Unwoven/Projects/Docker/gitlab_server/config/ssh_host_rsa_key*
-  fi
+  /usr/bin/sleep 2
+  /usr/bin/rm -rf /home/Unwoven/Projects/Docker/gitlab_server/logs/* /home/Unwoven/Projects/Docker/gitlab_server/data/* \
+  /home/Unwoven/Projects/Docker/gitlab_server/data/.* \
+  /home/Unwoven/Projects/Docker/gitlab_server/config/ssl /home/Unwoven/Projects/Docker/gitlab_server/config/trusted-certs \
+  /home/Unwoven/Projects/Docker/gitlab_server/config/gitlab-secrets.json /home/Unwoven/Projects/Docker/gitlab_server/config/initial_root_password \
+  /home/Unwoven/Projects/Docker/gitlab_server/config/ssh_host_ecdsa_key* /home/Unwoven/Projects/Docker/gitlab_server/config/ssh_host_ed25519_key* \
+  /home/Unwoven/Projects/Docker/gitlab_server/config/ssh_host_rsa_key*
 done
